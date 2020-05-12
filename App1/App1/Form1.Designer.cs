395ae72c4debec9d212df4e5eda29604dea0352f
kusa-mochi <whoatemyapplepie@gmail.com>
2020-05-12 18:36:09 +0900

    ComponentResourceManagerを使ってみたけど言語切り替えられない。

--- App1/App1/Form1.Designer.cs
+++ App1/App1/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.TestLabel = new System.Windows.Forms.Label();
             this.EnButton = new System.Windows.Forms.Button();
             this.JaButton = new System.Windows.Forms.Button();
@@ -36,57 +37,39 @@
             // 
             // TestLabel
             // 
-            this.TestLabel.AutoSize = true;
-            this.TestLabel.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.TestLabel.Location = new System.Drawing.Point(297, 94);
+            resources.ApplyResources(this.TestLabel, "TestLabel");
             this.TestLabel.Name = "TestLabel";
-            this.TestLabel.Size = new System.Drawing.Size(96, 34);
-            this.TestLabel.TabIndex = 0;
-            this.TestLabel.Text = "label1";
             // 
             // EnButton
             // 
-            this.EnButton.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.EnButton.Location = new System.Drawing.Point(159, 223);
+            resources.ApplyResources(this.EnButton, "EnButton");
             this.EnButton.Name = "EnButton";
-            this.EnButton.Size = new System.Drawing.Size(164, 60);
-            this.EnButton.TabIndex = 1;
-            this.EnButton.Text = "English";
             this.EnButton.UseVisualStyleBackColor = true;
             this.EnButton.Click += new System.EventHandler(this.EnButton_Click);
             // 
             // JaButton
             // 
-            this.JaButton.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.JaButton.Location = new System.Drawing.Point(377, 223);
+            resources.ApplyResources(this.JaButton, "JaButton");
             this.JaButton.Name = "JaButton";
-            this.JaButton.Size = new System.Drawing.Size(164, 60);
-            this.JaButton.TabIndex = 2;
-            this.JaButton.Text = "日本語";
             this.JaButton.UseVisualStyleBackColor = true;
             this.JaButton.Click += new System.EventHandler(this.JaButton_Click);
             // 
             // OpenModalButton
             // 
-            this.OpenModalButton.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.OpenModalButton.Location = new System.Drawing.Point(213, 324);
+            resources.ApplyResources(this.OpenModalButton, "OpenModalButton");
             this.OpenModalButton.Name = "OpenModalButton";
-            this.OpenModalButton.Size = new System.Drawing.Size(308, 85);
-            this.OpenModalButton.TabIndex = 3;
             this.OpenModalButton.UseVisualStyleBackColor = true;
             this.OpenModalButton.Click += new System.EventHandler(this.OpenModalButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.OpenModalButton);
             this.Controls.Add(this.JaButton);
             this.Controls.Add(this.EnButton);
             this.Controls.Add(this.TestLabel);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- App1/App1/Form1.cs
+++ App1/App1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,18 +25,27 @@ namespace App1
 
         private void EnButton_Click(object sender, EventArgs e)
         {
-
+            ChangeLanguage();
         }
 
         private void JaButton_Click(object sender, EventArgs e)
         {
-
+            ChangeLanguage("ja-JP");
         }
 
         private void OpenModalButton_Click(object sender, EventArgs e)
         {
             Form2 modal = new Form2();
             modal.ShowDialog(this);
+        }
+
+        private void ChangeLanguage(string lang = null)
+        {
+            foreach (Control c in this.Controls)
+            {
+                ComponentResourceManager resources = new ComponentResourceManager(typeof(Properties.Resources));
+                resources.ApplyResources(c, c.Name, lang == null ? CultureInfo.DefaultThreadCurrentCulture : new CultureInfo(lang));
+            }
         }
     }
 }
