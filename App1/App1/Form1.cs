d4ec8a12f74cbb02b09a05f40c5878d17113b710
kusa-mochi <whoatemyapplepie@gmail.com>
2020-05-12 19:42:14 +0900

    うーん、できない

--- App1/App1/Form1.cs
+++ App1/App1/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -24,18 +26,27 @@ namespace App1
 
         private void EnButton_Click(object sender, EventArgs e)
         {
-
+            ChangeLanguage();
         }
 
         private void JaButton_Click(object sender, EventArgs e)
         {
-
+            ChangeLanguage("ja-JP");
         }
 
         private void OpenModalButton_Click(object sender, EventArgs e)
         {
             Form2 modal = new Form2();
             modal.ShowDialog(this);
+        }
+
+        private void ChangeLanguage(string locale = null)
+        {
+            CultureInfo ci = locale == null ? CultureInfo.InvariantCulture : new CultureInfo(locale, false);
+            //Thread.CurrentThread.CurrentCulture = ci;
+            //Thread.CurrentThread.CurrentUICulture = ci;
+            CultureInfo.DefaultThreadCurrentCulture = ci;
+            CultureInfo.DefaultThreadCurrentUICulture = ci;
         }
     }
 }
